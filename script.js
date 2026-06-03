77731d84c1136591a0a613df9627c2427f2ab413
Satyam <satyamgkashid306@gmail.com>
2026-06-03 16:44:49 +0530

    Added form validation

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Portfolio Loaded");
+document.querySelector("form").addEventListener("submit", function(event){
+
+    let name = document.querySelector("input[type='text']").value;
+
+    if(name.trim() === ""){
+        alert("Please enter your name");
+        event.preventDefault();
+    }
+
+});
